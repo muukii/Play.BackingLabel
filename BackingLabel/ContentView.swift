a0b8719dadf0bf452708f0e1f612a8e8e43e0bc6
Muukii <muukii.app@gmail.com>
2019-10-05 17:28:39 +0900

    Initial Commit

---
+++ BackingLabel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackingLabel
+//
+//  Created by muukii on 2019/10/05.
+//  Copyright © 2019 muukii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
